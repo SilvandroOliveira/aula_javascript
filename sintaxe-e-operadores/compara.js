0690b4f41bc5bc8018f5ffc36373b01f776f9584
Silvandro Oliveira <silvandro_oliveira@hotmail.com>
2022-01-05 20:08:21 -0300

    Alterar padrao das constantes para SNAKE_UPPER_CASE

--- sintaxe-e-operadores/compara.js
+++ sintaxe-e-operadores/compara.js
@@ -1,10 +1,10 @@
 function comparaNumeros(num1, num2) {
-  const igual = num1 === num2 ? 'são' : 'não são';
-  const soma = num1 + num2;
-  const maior10 = soma > 10 ? 'maior' : 'menor';
-  const maior20 = soma > 20 ? 'maior' : 'menor';
+  const IGUAL = num1 == num2 ? 'são' : 'não são';
+  const SOMA = num1 + num2;
+  const MAIOR10 = SOMA > 10 ? 'maior' : 'menor';
+  const MAIOR20 = SOMA > 20 ? 'maior' : 'menor';
   alert(
-    `Os números ${num1} e ${num2} ${igual} iguais. Sua soma é ${soma}, que é ${maior10} que 10 e ${maior20} que 20.`
+    `Os números ${num1} e ${num2} ${IGUAL} iguais. Sua soma é ${SOMA}, que é ${MAIOR10} que 10 e ${MAIOR20} que 20.`
   );
 }
 
